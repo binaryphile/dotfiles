4599ac8b83ec39fd558b7438d59c1ee93c933893
Ted Lilley <ted.lilley@gmail.com>
2020-08-14 02:08:15 +0000

    basher

--- bash/apps/basher/env.bash
+++ bash/apps/basher/env.bash
@@ -1,1 +1,2 @@
 export BASHER_ROOT=$HOME/.local/opt/basher
+eval "$(basher init - bash)"
--- bash/apps/basher/init.bash
+++ bash/apps/basher/init.bash
@@ -1,1 +1,6 @@
-eval "$(basher init - bash)"
+source "$BASHER_ROOT"/lib/include.bash
+source "$BASHER_ROOT"/completions/basher.bash
+shopt -s nullglob
+for f in "$BASHER_ROOT"/cellar/completions/bash/*; do source "$f"; done
+shopt -u nullglob
+unset -v f
