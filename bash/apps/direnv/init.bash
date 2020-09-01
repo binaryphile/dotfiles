401c50e302bc924268a4c76891fc4e54f26f7de1
Ted Lilley <ted.lilley@gmail.com>
2020-09-01 03:55:45 +0000

    direnv

--- bash/apps/direnv/init.bash
+++ bash/apps/direnv/init.bash
@@ -1,5 +1,1 @@
-_direnv_hook() {
-  eval "$(direnv export bash)"
-}
-
-! StrContains $PROMPT_COMMAND _direnv_hook && PROMPT_COMMAND=$PROMPT_COMMAND${PROMPT_COMMAND+; }_direnv_hook
+eval "$(direnv hook bash)"
