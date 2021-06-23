4e656073e4f975bbc9fac9cea57b4804d93d109a
Ted Lilley <ted.lilley@digi.com>
2021-06-23 13:47:06 -0400

    ruby

---
+++ contexts/office/macos/bash/validate/apps/ruby.bash
@@ -0,0 +1,19 @@
+assertThat outputOf "ruby --version" isEqualTo "ruby 2.6.3p62 (2019-04-16 revision 67580) [x86_64-darwin18]"
+assertThat function rb exists
+
+assertThat aliases exist <<'END'
+  bemar
+  bemid
+  bemse
+  beraic
+  berais
+  berak
+  berakdm
+  bersp
+  betde
+  betru
+  bexec
+  binst
+  bupda
+  busou
+END
