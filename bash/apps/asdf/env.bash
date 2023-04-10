83cd934512126fff96b2ac5cd7dc2142c2cf31f3
Ted Lilley <ted.lilley@digi.com>
2023-04-10 12:55:25 -0400

    add deployments

--- bash/apps/asdf/env.bash
+++ bash/apps/asdf/env.bash
@@ -1,3 +1,4 @@
 TestAndExport CHARYBDIS_ROOT $HOME/GolandProjects/charybdis
+TestAndExport DEPLOY_ROOT /home/ted/project/charybdis-armt-deployments
 TestAndExport GOPATH $HOME/go
 TestContainsAndAppend PATH $HOME/go/bin
