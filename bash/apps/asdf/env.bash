5e1a0f75d502f5ce93a27fe4eb50710cf97d1770
Ted Lilley <ted.lilley@digi.com>
2023-11-08 15:21:53 -0500

    obsidian

--- bash/apps/asdf/env.bash
+++ bash/apps/asdf/env.bash
@@ -1,4 +1,2 @@
-TestAndExport CHARYBDIS_ROOT $HOME/GolandProjects/charybdis
-TestAndExport DEPLOY_ROOT /home/ted/project/charybdis-armt-deployments
 TestAndExport GOPATH $HOME/go
 TestContainsAndAppend PATH $HOME/go/bin
