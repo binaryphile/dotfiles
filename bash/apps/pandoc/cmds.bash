4b6262f690c41e748141aa8353abb4ce9b09e50d
Ted Lilley <ted.lilley@digi.com>
2025-01-14 14:28:12 -0500

    pandoc

--- bash/apps/pandoc/cmds.bash
+++ bash/apps/pandoc/cmds.bash
@@ -1,4 +1,4 @@
 shannon () {
   reveal "$FUNCNAME"
-  pandoc -f markdown -t markdown_github-hard_line_breaks$([[ $OSTYPE == darwin* ]] && echo +smart) --columns 92 --atx-headers -o "$1" "$1"
+  pandoc -f markdown -t gfm-hard_line_breaks$([[ $OSTYPE == darwin* ]] && echo +smart) --columns 92 --markdown-headings=atx -o "$1" "$1"
 }
