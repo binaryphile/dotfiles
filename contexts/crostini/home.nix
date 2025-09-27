c0ec808d49d67ff3be87aba00f26beaaacab1aa8
Ted Lilley <ted.lilley@digi.com>
2025-09-26 20:38:59 -0400

    ast-grep

--- contexts/crostini/home.nix
+++ contexts/crostini/home.nix
@@ -18,6 +18,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+    ast-grep
     claude-code
     coreutils
     diff-so-fancy
