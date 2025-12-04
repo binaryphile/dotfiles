839054297acf9b5e16b073904f3c740113112265
Ted Lilley <ted.lilley@digi.com>
2025-12-04 11:59:20 -0500

    transcript

--- bash/settings/env.bash
+++ bash/settings/env.bash
@@ -1,11 +1,12 @@
-TestContainsAndPrepend PATH $HOME/.local/lib
+TestContainsAndPrepend PATH ~/.local/lib
 TestContainsAndPrepend PATH /usr/local/bin
-TestContainsAndPrepend PATH $HOME/.local/bin
+TestContainsAndPrepend PATH ~/.local/bin
 
 TestCmdAndExport EDITOR nvim vim
 TestCmdAndExport PAGER less
 
-TestAndExport CFGDIR $HOME/.config
+TestAndExport CFGDIR ~/.config
+TestAndExport SECRETS ~/secrets
 TestAndExport XDG_CONFIG_HOME $CFGDIR
 
 export LC_SSH_USER=$USER
